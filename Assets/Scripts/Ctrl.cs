6a3d81fe8f43fb4ca1131c58d7fc9018d6658391
Sharpy <cam-chester@hotmail.co.uk>
2018-07-06 04:36:14 +0100

    Initial Commit

---
+++ Assets/Scripts/Ctrl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class Ctrl : MonoBehaviour {
+
+    Animator animator;
+
+    public float speed = 5f;
+
+
+    void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+
+	void Update () {
+        
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0f, 0f));
+        }
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime, 0f));
+        }
+        animator.SetFloat("X", Input.GetAxisRaw("Horizontal"));
+        animator.SetFloat("Y", Input.GetAxisRaw("Vertical"));
+    }
+
+
+
+    
+}
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Gun : MonoBehaviour {
+
+    public GameObject player;
+    public float speed = 5f;
+    GameObject pivot;
+
+	void Start () {
+        pivot = GameObject.FindGameObjectWithTag("Pivot");
+	}
+	
+
+	void Update () {
+
+        var mouse = Input.mousePosition;
+        var screenPoint = Camera.main.WorldToScreenPoint(transform.localPosition);
+        var offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
+        var angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+        pivot.transform.rotation = Quaternion.Euler(0, 0, angle);
+
+    }
+}
